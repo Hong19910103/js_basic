9d15f8e8f3852512dc2ffed4d9fe7b89887629ab
Hong19910103 <102145866+Hong19910103@users.noreply.github.com>
2022-03-29 17:35:49 +0700

    cautrucdieukien

--- bien_duleu_toantu/thuchanh_toantu/thuchanh_toantu.js
+++ bien_duleu_toantu/thuchanh_toantu/thuchanh_toantu.js
@@ -1,1 +1,9 @@
-let input
+
+let button = document.getElementById('result');
+button.addEventListener('click',data())
+function data() {
+    let input1 = parseInt(document.getElementById('width').value);
+    let input2 =parseInt(document.getElementById('height').value);
+    document.getElementById('Dientich').innerText = " Diện tích " + input1 * input2;
+    document.getElementById('chuvi').innerText = "chu vi" + ((input1 + input2)*2);
+}
